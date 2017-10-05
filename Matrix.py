1ba3b3c29bfd12eb88e90636337343fbb7e74215
Ryan Connell <rconnell2013@fau.edu>
2017-10-05 12:46:08 -0400

    added determinant and Minor functions

--- Matrix.py
+++ Matrix.py
@@ -7,10 +7,13 @@ def Id(n):
     return A
 
 class Matrix:
+    #BASE
     def __init__(self, n, m):
         self.crow = n
         self.ccol = m
         self.elements = list()
+        self.sq = (self.crow == self.ccol)
+        self.isRef = False
         for x in range(n):
             self.elements.append(list(range(m)))
             for y in range(m):
@@ -24,6 +27,7 @@ class Matrix:
             for y in range(self.ccol):
                 self.elements[x][y] = args[ref]
                 ref = ref + 1
+        self.isReg = (self.det() != 0)
                 
     def display(self):
         for i in range(self.crow):
@@ -31,7 +35,7 @@ class Matrix:
             for j in range(self.ccol):
                 print self.elements[i][j],
             print "]"
-
+    #HELPER FUNCTIONS
     def getElement(self, x, y):
         return self.elements[x][y]
 
@@ -39,6 +43,7 @@ class Matrix:
         self.elements[x][y] = n
         return None
 
+    #OPERATORS
     def __mul__(self, other):
 
         #if ( type(other) != Matrix ):
@@ -80,9 +85,74 @@ class Matrix:
     #ELEMENTARY OPERATIONS
     
     def row_multk(self,n,k): #n is row to be multiplied, k is constant to multiply by
-        temp = Id(self.ccol)
+        temp = Id(self.crow)
         temp.setElement(n,n,k)
         return temp * self
-    #def row_swap(self,n,m) #n and m are swapped rows
-    #def row_addk(self,n,m,k) #multiply row n by k and add it to m
+    def row_swap(self,n,m): #n and m are swapped rows
+        temp = Id(self.crow)
+        temp.setElement(n,m,1)
+        temp.setElement(m,n,1)
+        temp.setElement(n,n,0)
+        temp.setElement(m,m,0)
+        return temp * self
+    def row_addk(self,n,m,k): #multiply row n by k and add it to m
+        temp = Id(self.crow)
+        temp.setElement(m,n,k)
+        return temp * self
+
+    #MATRIX OPERATIONS
+
+    def Minor(self,i,j):
+        assert self.sq == True, "Matrix must be a square matrix"
+        temp = Matrix(self.crow - 1 , self.ccol - 1)
+        x = 0
+        y = 0
+        for m in range(self.crow):
+            for n in range(self.ccol):
+                if x < i:
+                    if y < j:
+                        temp.setElement(m,n,self.elements[x][y])
+                    if y > j:
+                        temp.setElement(m,n-1,self.elements[x][y])
+                if x > i:
+                    if y < j:
+                        temp.setElement(m-1,n,self.elements[x][y])
+                    if y > j:
+                        temp.setElement(m-1,n-1,self.elements[x][y])
+                y = y + 1
+            x = x + 1
+            y = 0
+        return temp
+    def det(self):
+        assert self.sq == True, "Matrix must be a square matrix"
+        temp = 0
+        if self.crow==2:
+            return (self.elements[0][0] * self.elements[1][1]) - (self.elements[0][1] * self.elements[1][0])
+        else:
+            for m in range(self.ccol):
+                temp = temp + (((-1)**m) * self.elements[0][m] * self.Minor(0,m).det())
+            return temp
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
 
